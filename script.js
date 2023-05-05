137fc0d44d59b620d0b0f122de3fb2230ed582b0
Dsalavarria <92231894+Dsalavarria@users.noreply.github.com>
2023-05-05 14:12:22 -0500

    revision segundo avance

---
+++ script.js
@@ -0,0 +1,51 @@
+const textArea = document.querySelector(".text-area");
+const mensaje=document.querySelector(".mensaje");
+
+function btnEncriptar()
+{
+    const textEncriptado= encriptar(textArea.value)
+    mensaje.value=textEncriptado
+    textArea.value="";
+    mensaje.stylebackgroundimage="none";
+}
+function encriptar(stringEncriptado)
+{
+    let matrizcodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringEncriptado = stringEncriptado.toLowerCase()
+
+    for(let i=0;i< matrizcodigo.length;i++)
+    {
+        if(stringEncriptado.includes(matrizcodigo[i][0]))
+        {
+            stringEncriptado=stringEncriptado.replaceAll(matrizcodigo[i][0],matrizcodigo[i][1])
+        }
+    }
+    return stringEncriptado
+}
+function desencriptar(stringDesencriptado)
+{
+    let matrizcodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    stringDesencriptado = stringDesencriptado.toLowerCase()
+
+    for(let i=0;i< matrizcodigo.length;i++)
+    {
+        if(stringDesencriptado.includes(matrizcodigo[i][1]))
+        {
+            stringDesencriptado=stringDesencriptado.replaceAll(matrizcodigo[i][1],matrizcodigo[i][0])
+        }
+    }
+    return stringDesencriptado
+}
+function btnDesencriptar()
+{
+    const textEncriptado= desencriptar(textArea.value)
+    mensaje.value=textEncriptado
+    textArea.value="";
+}
+function btncopiar()
+{
+  var textcopiado =document.getElementById(mensaje.value)
+  textcopiado.select()
+  document.axecCoommand('copy')
+  alert("texto copiado");
+}
